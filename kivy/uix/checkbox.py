8cce7760feaf1fdd7ea478b0ae25214fddcfe87b
MJ2P <mj2p.sg@gmail.com>
2014-09-25 13:58:07 +0100

    added kivy to project directory so that extra download and install is not needed

---
+++ kivy/uix/checkbox.py
@@ -0,0 +1,59 @@
+'''
+CheckBox
+========
+
+.. versionadded:: 1.4.0
+
+.. image:: images/checkbox.png
+    :align: right
+
+:class:`CheckBox` is a specific two-state button that can be either checked or
+unchecked. If the CheckBox is in a Group, it becomes a Radio button.
+As with the :class:`~kivy.uix.togglebutton.ToggleButton`, only one Radio button
+at a time can be selected when the :attr:`CheckBox.group` is set.
+
+An example usage::
+
+    from kivy.uix.checkbox import CheckBox
+
+    # ...
+
+    def on_checkbox_active(checkbox, value):
+        if value:
+            print('The checkbox', checkbox, 'is active')
+        else:
+            print('The checkbox', checkbox, 'is inactive')
+
+    checkbox = CheckBox()
+    checkbox.bind(active=on_checkbox_active)
+'''
+
+__all__ = ('CheckBox', )
+
+from kivy.uix.widget import Widget
+from kivy.properties import BooleanProperty
+from kivy.uix.behaviors import ToggleButtonBehavior
+
+
+class CheckBox(ToggleButtonBehavior, Widget):
+    '''CheckBox class, see module documentation for more information.
+    '''
+
+    active = BooleanProperty(False)
+    '''Indicates if the switch is active or inactive.
+
+    :attr:`active` is a :class:`~kivy.properties.BooleanProperty` and defaults
+    to False.
+    '''
+
+    def on_state(self, instance, value):
+        if value == 'down':
+            self.active = True
+        else:
+            self.active = False
+
+    def _toggle_active(self):
+        self._do_press()
+
+    def on_active(self, instance, value):
+        self.state = 'down' if value else 'normal'
